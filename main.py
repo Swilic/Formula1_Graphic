4f372546bbf20d72b558a274b1cae5d6facade79
Denis <Deniskazberuk@hotmail.com>
2023-02-01 02:31:41 +0100

    A big step forward. I forget to create a new branch -_-

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import fastf1 as ff1
 from fastf1 import plotting
-from fastf1 import plotting
+from matplotlib import pyplot as plt
 
-ff1.Cache.enable_cache('cache_dir')
+ff1.Cache.enable_cache('./cache')
+# FastF1's default color scheme
+ff1.plotting.setup_mpl()
 
 # Get the data
-laps = ff1.get_session(2021, 'Austria', 'Q').load_laps(with_telemetry=True)
+session = ff1.get_session(2021, 'Austria', 'Q')
+session.load(telemetry=True)
+hamilton = session.laps.pick_driver('HAM').pick_fastest().get_car_data().add_distance()
+speed_hamilton = hamilton['Speed']
+distance_hamilton = hamilton['Distance']
+
+# Plot the data
+fig, ax = plt.subplots()
+ax.plot(distance_hamilton, speed_hamilton, label='Hamilton', color=plotting.driver_color('ham'))
+ax.set_xlabel('Distance [m]')
+ax.set_ylabel('Speed [km/h]')
+fig.set_size_inches(16, 9)
+fig.set_dpi(100)
+
+plt.show()
